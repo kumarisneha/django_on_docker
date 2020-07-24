0ea8931dfd52bd978195ca848a92bdb7cd43eae2
kumarisneha <kumarisneha102@gmail.com>
2020-07-24 18:35:54 +0530

    Added some more urls

--- app/address_book/urls.py
+++ app/address_book/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from addrbookapp import views
+from upload.views import image_upload
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('create/', views.create, name ='create'),
+    path('delete/<int:id>/',views.delete_contact, name ='delete_contact'),
+    path('update/<int:id>/',views.update_contact, name ='update_contact'),
+    path('update_page/<int:id>/',views.update_page, name ='update_page'),
+    path("upload/image/", image_upload, name="upload"),
+    path("admin/", admin.site.urls),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
