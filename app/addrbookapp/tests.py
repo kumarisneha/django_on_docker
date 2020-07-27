78ec241ab72ee7daf16f7dd72cf62a62d0143fce
kumarisneha <kumarisneha102@gmail.com>
2020-07-27 18:22:56 +0530

    added test cases

--- app/addrbookapp/tests.py
+++ app/addrbookapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+# from addrbookapp.models import Address
 
-# Create your tests here.
+# class AddressTestCase(TestCase):
+#     def setUp(self):
+#         Address.objects.create(user="sneha", address="patna",
+#         	email_id="xyz@gmail.com", phone_number="2345678901")
+
+#     def test_user_add_email_phone_no(self):
+#         '''Get an Address object to test'''
+#         addr = Address.objects.get(id=1)
+#         print(addr)
+#         self.assertEquals(addr.user, 'sneha')
+#         self.assertEquals(addr.email_id, 'xyz@gmail.com')
+#         self.assertEquals(addr.address, 'patna')
+#         self.assertEquals(addr.phone_number, '2345678901')
